b8fec3d2899346340db8eb0664a2d90e7fc66fb3
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2023-02-01 20:55:01 +0100

    Expand about page

--- source/monophony/frontend/windows/main_window.py
+++ source/monophony/frontend/windows/main_window.py
@@ -75,8 +75,36 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
 		)
+		win_about.add_legal_section(
+			'ytmusicapi',
+			'Copyright (c) 2020 sigma67',
+			Gtk.License.CUSTOM,
+			'''Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
+		)
+		win_about.add_legal_section(
+			'SponsorBlock',
+			'',
+			Gtk.License.CUSTOM,
+			'Uses SponsorBlock data licensed used under CC BY-NC-SA 4.0 from https://sponsor.ajay.app/.'
+		)
 		win_about.set_translator_credits(_('translator-credits'))
-		win_about.add_credit_section(_('Patrons'), ['yuanca'])
+		win_about.add_acknowledgement_section(_('Patrons'), ['yuanca'])
 		win_about.set_issue_url('https://gitlab.com/zehkira/monophony/-/issues')
 		win_about.add_link(_('Donate'), 'https://www.patreon.com/zehkira')
 		win_about.set_website('https://gitlab.com/zehkira/monophony')
