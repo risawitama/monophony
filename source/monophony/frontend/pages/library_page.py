07d0d4519d4fc68e598a97fc6c002cd76c609fd4
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2023-02-14 09:33:38 +0100

    Fix library page not expanding vertically

--- source/monophony/frontend/pages/library_page.py
+++ source/monophony/frontend/pages/library_page.py
@@ -28,6 +28,7 @@ class MonophonyLibraryPage(Gtk.Box):
 		self.btn_play = Gtk.Button.new_with_label(_('Play all'))
 		self.btn_play.connect('clicked', self._on_play_all)
 		self.box_playlists = Adw.PreferencesGroup()
+		self.box_playlists.set_vexpand(True)
 		self.box_playlists.set_title(_('Playlists'))
 		self.box_playlists.set_header_suffix(self.btn_play)
 		self.box_meta.add(self.box_playlists)
