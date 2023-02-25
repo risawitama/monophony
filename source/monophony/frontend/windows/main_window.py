5a8d3405c21e3b7f3fa8acce1c559435d830585e
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2023-02-25 19:02:51 +0100

    Add requests license

--- source/monophony/frontend/windows/main_window.py
+++ source/monophony/frontend/windows/main_window.py
@@ -133,6 +133,9 @@ SOFTWARE.'''
 		win_about.add_legal_section(
 			'mpris_server', '', Gtk.License.AGPL_3_0, None
 		)
+		win_about.add_legal_section(
+			'requests', '', Gtk.License.APACHE_2_0, None
+		)
 		win_about.set_translator_credits(_('translator-credits'))
 		win_about.set_issue_url('https://gitlab.com/zehkira/monophony/-/issues')
 		win_about.add_link(_('Donate'), 'https://ko-fi.com/zehkira')
