2ea3f06bc0680c016aae73a0c235b00cdd337a1c
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2023-05-27 15:36:39 +0200

    Make translation not fail regardless of container

--- source/bin/monophony.py
+++ source/bin/monophony.py
@@ -10,20 +10,8 @@ def main():
 	path = None
 	if os.getenv('container', '') == 'flatpak':
 		path = '/app/share/locale'
-		print('Container: Flatpak')
-	else:
-		snap_path = os.getenv('SNAP')
-		if snap_path:
-			path = snap_path
-			print('Container: Snap')
-		else:
-			print('Container: None')
 
-	try:
-		gettext.translation('monophony', path).install()
-	except FileNotFoundError:
-		gettext.translation('monophony', path, languages = ['en']).install()
-
+	gettext.translation('monophony', path, fallback = True).install()
 	monophony.backend.cache.clean_up()
 	MonophonyApplication().run()
 
