730c86ee9aab7a52647c3272d2b163a6dc0e05b1
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2023-06-10 13:54:02 +0200

    Make song is-downloading check reliable

--- source/monophony/backend/cache.py
+++ source/monophony/backend/cache.py
@@ -6,10 +6,26 @@ from gi.repository import GLib
 def is_song_being_cached(video_id: str) -> bool:
 	music_dir = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_MUSIC)
 	if music_dir:
+		has_temp = False
+		has_part = False
+		has_result = False
 		for file in os.listdir(music_dir + '/monophony/'):
 			parts = file.split('.')
-			if parts[0] == video_id and parts[-1] == 'part':
-				return True
+			if parts[0] == video_id:
+				if parts[-1] == 'temp':
+					has_temp = True
+				elif parts[-1] == 'part':
+					has_part = True
+				elif parts[-1] == video_id:
+					has_result = True
+
+		if not has_result:
+			return has_temp
+		elif not has_part:
+			try:
+				os.remove(f'{music_dir}/monophony/{video_id}.temp')
+			except (OSError, FileNotFoundError):
+				pass
 
 	return False
 
@@ -44,6 +60,7 @@ def cache_song(video_id: str):
 		return
 	path += '/monophony/'
 	os.makedirs(path, exist_ok = True)
+	open(f'{path}/{video_id}.temp', 'w').close()
 
 	out, _ = subprocess.Popen(
 		f'yt-dlp -x --no-cache-dir --audio-quality 0 --add-metadata -o "{path}/%(id)s.%(ext)s" https://music.youtube.com/watch?v={video_id}',
