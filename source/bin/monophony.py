7bb4eb69379733974c8d39b2ef4c3e59d95b7812
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2023-09-29 09:35:49 +0000

    Add locale path for snaps zehkira/monophony!55 Closes #113

--- source/bin/monophony.py
+++ source/bin/monophony.py
@@ -8,9 +8,11 @@ from monophony.frontend.app import MonophonyApplication
 
 def main():
 	path = None
+	snap_path = os.getenv('SNAP')
 	if os.getenv('container', '') == 'flatpak':
 		path = '/app/share/locale'
-
+	elif snap_path:
+		path = os.path.join(snap_path, 'share/locale')
 	gettext.translation('monophony', path, fallback=True).install()
 	monophony.backend.cache.clean_up()
 	MonophonyApplication().run()
