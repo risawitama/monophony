5d388ac7f9b83cf125a1c1cc7e538333c34c63c1
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2024-03-23 12:17:24 +0100

    Inhibit idle and suspend #137

--- source/monophony/frontend/app.py
+++ source/monophony/frontend/app.py
@@ -3,7 +3,8 @@ from monophony.frontend.windows.main_window import MonophonyMainWindow
 
 import gi
 gi.require_version('Adw', '1')
-from gi.repository import Adw, Gio
+gi.require_version('Gtk', '4.0')
+from gi.repository import Adw, Gio, Gtk
 
 
 class MonophonyApplication(Adw.Application):
@@ -16,5 +17,10 @@ class MonophonyApplication(Adw.Application):
 	def do_activate(self):
 		self.window = MonophonyMainWindow(
 			application = self
+		)
+		self.inhibit(
+			self.window,
+			Gtk.ApplicationInhibitFlags.SUSPEND | Gtk.ApplicationInhibitFlags.IDLE,
+			None
 		)
 		self.window.present()
